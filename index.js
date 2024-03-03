ae54ff5b27ac048677506b2751566153467c80f0
pearbearr <125711734+pearbearr@users.noreply.github.com>
2024-03-03 17:13:49 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const files = ["index.1html", "index2.html"];
+let num = 0;
+function buttonclick() {
+    var news = num + 1;
+    window.location.assign("index"+ news +".html")
+
+}
+function buttonclick2() {
+    var news = num + 2;
+    window.location.assign("index"+ news +".html")
+}
+function buttonclick3() {
+    window.location.assign("index0.html")
+}
+// function shake() {
+//     indow.moveBy(1,0)
+//     indow.moveBy(-1,0)
+//     indow.moveBy(-1,0)
+//     indow.moveBy(1,0)
+// const text = document.querySelector("text");
+// text.addEventListener("Objective: Save Princess Tim", then());
+
+
+function shake() {
+        document.querySelector("#button2").style.animation ="shake .1s 4"
+
+
+    
+}
+function shake2() {
+    document.querySelector("#button3").style.animation ="shake .1s 4"
+
+
+}
+function shake3() {
+    
+    document.querySelector("#button4").style.animation ="shake .1s 4"
+
+
+}
+function shake4() {
+    document.querySelector("#button5").style.animation ="shake .1s 4"
+
+
+}
+function shake5() {
+    document.querySelector("#button7").style.animation ="shake .1s 4"
+
+
+}
+function shake6() {
+    document.querySelector("#button8").style.animation ="shake .1s 4"
+
+
+}
